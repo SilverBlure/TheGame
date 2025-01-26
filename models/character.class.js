37fef6708204a953d0feab9b3c62716d9bbec78f
Nicolai <oesterle.ni@onlinehome.de>
2025-01-26 10:55:58 +0100

    change index to de

--- models/character.class.js
+++ models/character.class.js
@@ -2,9 +2,11 @@ class Character extends MovableObject{
     width = 250;
     height = 200;
     y = 150;
+    img;
+    imgCach = {};
    constructor(){
     super().loadImage('./../assets/1.Sharkie/1.IDLE/1.png',);
-    
+    this.loadImage()
    }
 
     jump() {
--- models/enemie.class.js
+++ models/enemie.class.js
@@ -9,8 +9,6 @@ class Enemie extends MovableObject {
 
     constructor() {
         super().loadImage('./../assets/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
-
-
         this.x = 250 + Math.random() * 500;
         
     }
