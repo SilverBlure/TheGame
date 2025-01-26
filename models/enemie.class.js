033e988284aa4a71fb9c4f0a2cf2c6fefce137f0
Nicolai <oesterle.ni@onlinehome.de>
2025-01-26 10:48:15 +0100

    new enemie positioning

--- models/enemie.class.js
+++ models/enemie.class.js
@@ -3,19 +3,21 @@ class Enemie extends MovableObject {
     heigth = 100;
     x;
 
+    log(){
+        console.log(this.x);
+    }
+
     constructor() {
         super().loadImage('./../assets/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
 
 
-        this.x = Math.random() * 500;
-        this.x = x;
-        log(x);
+        this.x = 250 + Math.random() * 500;
+        
     }
+
 
     moveLeft() {
 
     }
-    log(x){
-        console.log(x);
-    }
+    
 }   
