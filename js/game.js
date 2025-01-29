38d556f30e6f77d095f277072b22790df8b62f1d
Nicolai <oesterle.ni@onlinehome.de>
2025-01-29 18:36:11 +0100

    add keylist

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 let canvas;
 let ctx;
 let world;
+let keyboard = new Keyboard();
 
 
 
@@ -9,6 +10,26 @@ function init(){
     world = new World(canvas);
 }
 
-window.addEventListener('keydown',(event) =>{
-    console.log(event);
+window.addEventListener('keydown',(e) =>{
+    if(e.keyCode ==37){
+        keyboard.LEFT = true;
+    }
+    if(e.keyCode == 39){
+        keyboard.RIGHT = true;
+    }
+    if(e.keyCode == 38){
+        keyboard.UP = true;
+    }
+    if(e.keyCode == 40){
+        keyboard.DOWN = true;
+    }
+    if(e.keyCode == 32){
+        keyboard.SPACE = true;
+    }
+    if(e.keyCode == 65){
+        keyboard.A = true;
+    }
+    if(e.keyCode == 83){
+        keyboard.S = true;
+    }
 })
--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,11 +1,12 @@
 class Keyboard {
-left = false;
-right = false;
-up = false;
-down = false;
-space = false;
-a = false; //attack mele
-s = false; //attack range
+
+    LEFT = false;   //keycode 37
+    RIGHT = false; //keycode 39
+    UP = false; //keycode 38
+    DOWN = false;   //keycode 40
+    SPACE = false;  //keycode 32
+    A = false; //attack mele 65
+    S = false; //attack range 83
 
 
 
