6fe569aae80893ad95c5fe861b558e1b24938f39
Nicolai <oesterle.ni@onlinehome.de>
2025-03-09 16:16:30 +0100

    add img

---
+++ models/menu.class.js
@@ -0,0 +1,29 @@
+class Menue extends DrawableObject{
+
+    GAMEOVER_IMG = [
+        'assets/6.Botones/Tittles/Game Over/Recurso 9.png',
+        'assets/6.Botones/Tittles/Game Over/Recurso 10.png',
+        'assets/6.Botones/Tittles/Game Over/Recurso 11.png',
+        'assets/6.Botones/Tittles/Game Over/Recurso 12.png',
+        'assets/6.Botones/Tittles/Game Over/Recurso 13.png',
+    ];
+    YOUWIN_IMG = [
+        'assets/6.Botones/Tittles/You win/Recurso 19.png',
+        'assets/6.Botones/Tittles/You win/Recurso 20.png',
+        'assets/6.Botones/Tittles/You win/Recurso 21.png',
+        'assets/6.Botones/Tittles/You win/Recurso 22.png',
+    ];
+
+    
+
+img 
+buttons
+    start
+    controlls
+    fullscreen
+    try again 
+
+    constructor(){
+
+    }
+}
