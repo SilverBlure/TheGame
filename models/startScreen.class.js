c76e8830d595a4eb061d1c3b883ecc78f2edb855
Nicolai <oesterle.ni@onlinehome.de>
2025-03-10 09:14:05 +0100

    add imges

---
+++ models/startScreen.class.js
@@ -0,0 +1,53 @@
+class StartScreen extends DrawableObject{
+    x;
+    y;
+    height;
+    width;
+    ImgSrc;
+    
+
+    GAMEOVER_IMG = [
+        'assets/6.Botones/Tittles/Game Over/Recurso 9.png',
+        'assets/6.Botones/Tittles/Game Over/Recurso 10.png',
+        'assets/6.Botones/Tittles/Game Over/Recurso 11.png',
+        'assets/6.Botones/Tittles/Game Over/Recurso 12.png',
+        'assets/6.Botones/Tittles/Game Over/Recurso 13.png',
+    ];
+    YOUWIN_IMG = [
+        'assets/6.Botones/Tittles/You win/Recurso 19.png',
+        'assets/6.Botones/Tittles/You win/Recurso 20.png',
+        'assets/6.Botones/Tittles/You win/Recurso 21.png',
+        'assets/6.Botones/Tittles/You win/Recurso 22.png',
+    ];
+
+    TRYAGAIN_IMG = [
+        'assets/6.Botones/Try again/Recurso 15.png',
+        'assets/6.Botones/Try again/Recurso 16.png',
+        'assets/6.Botones/Try again/Recurso 17.png',
+        'assets/6.Botones/Try again/Recurso 18.png',
+    ];
+
+    FULLSCREEN_IMG = ['assets/6.Botones/Full Screen/Mesa de trabajo 6.png',
+        'assets/6.Botones/Full Screen/Mesa de trabajo 7.png',
+        'assets/6.Botones/Full Screen/Mesa de trabajo 8.png',
+        'assets/6.Botones/Full Screen/Mesa de trabajo 9.png',
+    ]
+
+    ARROW_KEYS_IMG = 'assets/6.Botones/Key/arrow keys.png';
+
+    D_KEY_IMG = 'assets/6.Botones/Key/D key.png';
+
+    SPACEBAR_KEY_IMG = 'assets/6.Botones/Key/Space Bar key.png';
+
+img 
+buttons
+    start
+    controlls
+    fullscreen
+    try again
+    mouse position
+
+    constructor(x, y, height, width, imgSrc){
+
+    }
+}
