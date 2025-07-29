f81383952893218a741933c132b7a38c73ea1855
SilverBlure <oesterle.ni@onlinehome.de>
2025-07-29 16:37:08 +0200

    remove old code and start dokumentation

--- models/background.class.js
+++ models/background.class.js
@@ -1,7 +1,21 @@
+/**
+ * Crate an new Background Object
+ * @class
+ * inhert from MovableObject
+ */
+
 class Background extends MovableObject {
     width = 720;
     height = 480;
 
+    /**
+     * 
+     * @constructor
+     * Create a background Object
+     * @param {string} imagePath - relativ path of the Image
+     * @param {number} x - x coordinate for the Image
+     * @param {number} y - y coordinare for the Image
+     */
     constructor(imagePath, x, y) {
         super();
         this.loadImage(imagePath);
--- models/button.class.js
+++ models/button.class.js
@@ -1,14 +1,24 @@
-class Button extends DrawableObject {
-    height;
-    width;
-    x;
-    y;
-    constructor(start, fullScreen) {
-        super();
-        this.start = start;
-        this.fullScreen = fullScreen;
-        this.loadImage(this.start);
-        this.loadImage(this.fullScreen);
+// /**
+//  * @class
+//  * inhert from DarableObject
+//  */
+// class Button extends DrawableObject {
+//     height;
+//     width;
+//     x;
+//     y;
 
-    }
-}
+// /**
+//  * 
+//  * @param {*} start 
+//  * @param {*} fullScreen 
+//  */
+//     constructor(start, fullScreen) {
+//         super();
+//         this.start = start;
+//         this.fullScreen = fullScreen;
+//         this.loadImage(this.start);
+//         this.loadImage(this.fullScreen);
+
+//     }
+// }
